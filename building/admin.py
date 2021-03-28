16f9b277d4f365d763d1b47f4ff19ed2f5515f33
Bogdan Aldea <bogdan.aldea0623@gmail.com>
2021-03-28 14:43:07 -0700

    registered models for django admin panel

--- building/admin.py
+++ building/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register(User)
+admin.site.register(Building)
+admin.site.register(Apartment)
+
+admin.site.register(Utility)
+admin.site.register(MutualUtil)
+admin.site.register(IndividualUtil)
+
+admin.site.register(Feature)
+admin.site.register(FeatureLinked)
