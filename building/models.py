8db57e71481d0cbd69c67ced2dd4e6f7cf741952
Bogdan Aldea <bogdan.aldea0623@gmail.com>
2021-03-28 14:48:13 -0700

    fixed typos

--- building/models.py
+++ building/models.py
@@ -46,12 +46,12 @@ class Utility(models.Model):
         ('Private', 'Private')
     )
 
-    name = models.CharField(max_length=50, null=True)
-    provider = models.CharField(max_length=50, choices=PROVIDERS)
-    contract_starts = models.DateField(null=True, blank=True)
-    contract_ends = models.DateField(null=True, blank=True)
-    util_tax_wage = models.FloatField(default=0, null=True)
-    building = models.ForeignKey(Building, on_delete=models.CASCADE)
+    name                = models.CharField(max_length=50, null=True)
+    provider            = models.CharField(max_length=50, choices=PROVIDERS)
+    contract_starts     = models.DateField(null=True, blank=True)
+    contract_ends       = models.DateField(null=True, blank=True)
+    util_tax_wage       = models.FloatField(default=0, null=True)
+    building            = models.ForeignKey(Building, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -63,9 +63,9 @@ with the pre defined type of 'mutual'.
 """
 class MutualUtil(models.Model):
 
-    apartment = models.ForeignKey(Apartment, null=True, on_delete=models.CASCADE)
-    common_util = models.ForeignKey(Utility, null=True, on_delete=models.CASCADE)
-    util_type = models.CharField(default='Mutual', max_length=20)
+    apartment       = models.ForeignKey(Apartment, null=True, on_delete=models.CASCADE)
+    common_util     = models.ForeignKey(Utility, null=True, on_delete=models.CASCADE)
+    util_type       = models.CharField(default='Mutual', max_length=20)
 
     def __str__(self):
         return f'{self.common_util.name}: {self.apartment}'
@@ -82,9 +82,9 @@ class IndividualUtil(models.Model):
         (False, 'Disabled')
     )
 
-    apartment = models.ForeignKey(Apartment, null=True, on_delete=models.CASCADE)
-    individual_util = models.ForeignKey(Utility, null=True, on_delete=models.CASCADE)
-    util_type = models.CharField(default='Individual', max_length=20)
+    apartment           = models.ForeignKey(Apartment, null=True, on_delete=models.CASCADE)
+    individual_util     = models.ForeignKey(Utility, null=True, on_delete=models.CASCADE)
+    util_type           = models.CharField(default='Individual', max_length=20)
 
     def __str__(self):
         return f'{self.individual_util.name}: {self.apartment}'
@@ -93,9 +93,9 @@ class IndividualUtil(models.Model):
 # Defined feature model
 class Feature(models.Model):
 
-    name = models.CharField(max_length=50, null=True)
-    feature_tax = models.FloatField(default=0, null=True)
-    building = models.ForeignKey(Building, null=True, on_delete=models.CASCADE)
+    name            = models.CharField(max_length=50, null=True)
+    feature_tax     = models.FloatField(default=0, null=True)
+    building        = models.ForeignKey(Building, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -104,8 +104,8 @@ class Feature(models.Model):
 # Defines feature -> apartment intermediary model. Each model points to a created feature object
 class FeatureLinked(models.Model):
 
-    apartment = models.ForeignKey(Apartment, null=True, on_delete=models.CASCADE)
-    related_feature = models.ForeignKey(Feature, null=True, on_delete=models.CASCADE)
+    apartment           = models.ForeignKey(Apartment, null=True, on_delete=models.CASCADE)
+    related_feature     = models.ForeignKey(Feature, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.related_feature.name}: {self.apartment}'
