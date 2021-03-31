42ff9ac86329078514e7eebce3b7677b1aabb7ab
Bogdan Aldea <bogdan.aldea0623@gmail.com>
2021-03-31 15:22:11 -0700

    added urls for login, logout & residential creation

--- building/urls.py
+++ building/urls.py
@@ -8,4 +8,10 @@ urlpatterns = [
     path('settings/', views.admin_settings, name='settings'),
     path('counters/', views.admin_counters, name='counters'),
     path('payments/', views.admin_payments, name='payments'),
+
+    path('register/', views.register_page, name='register'),
+    path('login/', views.login_page, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+
+    path('create-residential/', views.add_residential, name='create_residential')
 ]
