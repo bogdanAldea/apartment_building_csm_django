5c92300afd0283385b360fe2fdfb816147cc4665
Bogdan Aldea <bogdan.aldea0623@gmail.com>
2021-04-15 02:43:35 -0700

    added decorator to login % registration form that restricts pages for already logged in users

--- residential/views/auth.py
+++ residential/views/auth.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import Group
 from residential.forms import *
+from residential.decorators import unauthenticated_user
 
 
+@unauthenticated_user
 def RegisterPage(request):
     """
     Defined view that handles new user registration with admin privileges.
@@ -28,6 +30,7 @@ def RegisterPage(request):
     return render(request, 'residential/registration/register.html', context)
 
 
+@unauthenticated_user
 def LoginPage(request):
     """
     Defined view that handles the user login (user with admin privileges).
@@ -36,8 +39,9 @@ def LoginPage(request):
     # check type of request method
     if request.method == 'POST':
         # retrieve user's login credentials
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+        username    = request.POST.get('username')
+        password    = request.POST.get('password')
+        group       = request.POST.get('groups')
 
         # attempt user authentication
         user = authenticate(request, username=username, password=password)
