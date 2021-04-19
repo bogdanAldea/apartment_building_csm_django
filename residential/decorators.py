a61fb7165524afff2bee6439dc6a60daa3798de7
Bogdan Aldea <bogdan.aldea0623@gmail.com>
2021-04-19 17:02:52 +0200

    restrict views for unauthorized users

--- residential/decorators.py
+++ residential/decorators.py
@@ -10,3 +10,21 @@ def unauthenticated_user(view_function):
             return view_function(request, *args, **kwargs)
 
     return wrapper
+
+
+def allowed_users(user_roles: list, redirect_link: str):
+    def decorator(view_function):
+        def wrapper(request, *args, **kwargs):
+
+            group = None
+            # check if user is assigned to a specific group
+            if request.user.groups.exists():
+                group = request.user.groups.all()[0].name
+
+            # if user meets the user role description -> allow page to be accessed
+            if group in user_roles:
+                return view_function(request, *args, **kwargs)
+            else:
+                return redirect(redirect_link)
+        return wrapper
+    return decorator
