57c59f8156310c8626a20ad039b63b014b33c01a
Bogdan <bogdan.aldea0623@gmail.com>
2021-06-25 16:27:43 +0300

    created building app

---
+++ building/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import menu
+
+app_name = 'building'
+
+urlpatterns = [
+
+    # sidebar urls
+    path("", menu.DashboardPage, name="admin_dashboard"),
+    path("settings/", menu.SettingsPage, name="admin_settings"),
+    path("apartments/", menu.ApartmentsPage, name="admin_apartments"),
+    path("payments/", menu.PaymentsPage, name="admin_payments"),
+
+]
---
+++ building/views/menu.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+
+def DashboardPage(request):
+    return render(request, 'building/menu/admin_dashboard.html')
+
+
+def SettingsPage(request):
+    return render(request, 'building/menu/admin_settings.html')
+
+
+def ApartmentsPage(request):
+    return render(request, 'building/menu/admin_apartments.html')
+
+
+def PaymentsPage(request):
+    return render(request, 'building/menu/admin_payments.html')
