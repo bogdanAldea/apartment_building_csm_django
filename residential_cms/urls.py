5c150bc91436984e2e0029e6c7da141cfe8f3e60
Bogdan <bogdan.aldea0623@gmail.com>
2021-06-26 15:28:08 +0300

    set up apartments url path

--- residential_cms/urls.py
+++ residential_cms/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('residential.urls', namespace='building'))
+    path('', include('building.urls', namespace='building')),
+    path('apartments/', include('apartment.urls', namespace='apartment')),
 ]
